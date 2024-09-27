43eb91bfb3caffb6587ac00e8d3aa2459c12c116
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2024-09-27 20:24:51 +0700

    feat(algorithm_test): clear all algorithm test

--- algorithms_test/test1.js
+++ algorithms_test/test1.js
@@ -1,6 +1,7 @@
 function reverseStringWithNumberLoop(str) {
   let reversed = "";
   let number = "";
+
   for (let i = str.length - 1; i >= 0; i--) {
     if (isNaN(str[i])) {
       reversed += str[i];
@@ -11,6 +12,4 @@ function reverseStringWithNumberLoop(str) {
   return reversed + number;
 }
 
-const string = "NEGIE1";
-const result = reverseStringWithNumberLoop(string);
-console.log(result);
+console.log(reverseStringWithNumberLoop("NEGIE1"));
